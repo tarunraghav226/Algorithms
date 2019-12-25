c386f72df1cda9fef2acd1fc81895b495d0c5285
tarunraghav226 <tarun.raghav226@gmail.com>
2019-12-25 15:12:47 +0530

    added main class

---
+++ minimaxAlgo/src/Main.java
@@ -0,0 +1,14 @@
+import minimax.MinimaxAlgo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String game = scan.next();
+        MinimaxAlgo algo = new MinimaxAlgo();
+        algo.setStateOfGame(game);
+        algo.bestMove();
+        System.out.println(algo.toString());
+    }
+}
