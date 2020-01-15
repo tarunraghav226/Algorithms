2ed2cdc0cef858e214e42eab2d808eac4be612dc
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-15 12:46:22 +0530

    Final Commit.

--- euclidsAlgo/src/Main.java
+++ euclidsAlgo/src/Main.java
@@ -1,2 +1,13 @@
+import euclidsAlgo.EuclidsAlgorithm;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        EuclidsAlgorithm euclidsAlgorithm = new EuclidsAlgorithm();
+        Scanner scan = new Scanner(System.in);
+        euclidsAlgorithm.setFirstNumber(scan.nextInt());
+        euclidsAlgorithm.setSecondNumber(scan.nextInt());
+        System.out.println("GCD of given two numbers is --> " + euclidsAlgorithm.getGcd());
+    }
 }
