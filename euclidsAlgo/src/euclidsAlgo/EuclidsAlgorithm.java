8957ae360e0359e53c09ad13e183be54786308fb
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-15 12:30:51 +0530

    Added setters.

--- euclidsAlgo/src/euclidsAlgo/EuclidsAlgorithm.java
+++ euclidsAlgo/src/euclidsAlgo/EuclidsAlgorithm.java
@@ -1,4 +1,16 @@
 package euclidsAlgo;
 
 public class EuclidsAlgorithm {
+    private int firstNumber;
+    private int secondNumber;
+    private int gcd;
+
+    public void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
 }
