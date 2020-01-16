c4155578bbdd96725d9c653651c76e3d4f7e7d11
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-16 18:24:24 +0530

    Added bubble sort code.

--- bubbleSort/src/bubbleSort/BubbleSort.java
+++ bubbleSort/src/bubbleSort/BubbleSort.java
@@ -1,4 +1,21 @@
 package bubbleSort;
 
 public class BubbleSort {
+
+    public <T extends Comparable> void bubbleSort(T[] arr) {
+        boolean flag;
+        for (int i = 0; i < arr.length - 1; i++) {
+            flag = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    T temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j] = temp;
+                    flag = true;
+                }
+            }
+            if (flag == false)
+                return;
+        }
+    }
 }
