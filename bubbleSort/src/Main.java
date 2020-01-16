d2b78b7d0f1c8f86e11dc70eabbabc80c9986a07
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-16 18:44:31 +0530

    Completed Main java file.

--- bubbleSort/src/Main.java
+++ bubbleSort/src/Main.java
@@ -1,2 +1,19 @@
+import bubbleSort.BubbleSort;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int size = scan.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = scan.nextInt();
+        }
+        BubbleSort.bubbleSort(arr);
+        for (Integer integer : arr) {
+            System.out.println(integer);
+        }
+    }
 }
