5db51f6953bd3c8748ceaa5413d6a6de4fd72911
tarunraghav226 <tarun.raghav226@gmail.com>
2020-03-02 17:18:57 +0530

    Completed code for insertion sort via array and a new value.

--- insertionSort/src/insertionSorting/InsertionSort.java
+++ insertionSort/src/insertionSorting/InsertionSort.java
@@ -1,4 +1,18 @@
 package insertionSorting;
 
 public class InsertionSort {
+
+    public static void sort(int[] arr, int length, int val) {
+        if (length == 0) {
+            arr[0] = val;
+            return;
+        }
+        int j = length - 1;
+        while (j >= 0 && arr[j] > val) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = val;
+    }
 }
+
