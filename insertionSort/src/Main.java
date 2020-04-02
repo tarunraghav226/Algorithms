fb8aeceec7507e7c1db131657abd298f813f2b8f
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-02 15:33:51 +0530

    Added insertion sort using loop.

--- insertionSort/src/Main.java
+++ insertionSort/src/Main.java
@@ -1,11 +1,18 @@
 import insertionSorting.InsertionSort;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter length of array: ");
+        int n = scan.nextInt();
         int[] arr = new int[5];
-        for (int i = 0; i < 5; i++)
-            arr[i] = 5 - i;
-        InsertionSort.sort(arr, 5);
+        for (int i = 0; i < 5; i++){
+            System.out.print("Enter data: ");
+            arr[i]=scan.nextInt();
+        }
+        InsertionSort.sort(arr);
+        System.out.println("Array after sorting --- ");
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
--- insertionSort/src/insertionSorting/InsertionSort.java
+++ insertionSort/src/insertionSorting/InsertionSort.java
@@ -2,6 +2,7 @@ package insertionSorting;
 
 public class InsertionSort {
 
+    //insertion sort at te time of initialization of array
     public static void sort(int[] arr, int length, int val) {
         if (length == 0) {
             arr[0] = val;
@@ -15,6 +16,7 @@ public class InsertionSort {
         arr[j + 1] = val;
     }
 
+    //insertion sort using recursion
     public static void sort(int[] arr, int size) {
         if (size <= 1)
             return;
@@ -28,6 +30,19 @@ public class InsertionSort {
             j--;
         }
         arr[j + 1] = end;
+    }
+
+    //insertion sort using loops
+    public static void sort(int[] arr){
+        for(int i=1;i<arr.length;i++){
+            int endVal=arr[i];
+            int j=i-1;
+            while (j>=0 && endVal<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=endVal;
+        }
     }
 }
 
