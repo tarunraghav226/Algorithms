81499803d759ef940c820e1340ee69ebdd6d5cda
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-02 13:56:34 +0530

    Added selection sort logic.

--- selectionSort/src/selectionSort/SelectionSort.java
+++ selectionSort/src/selectionSort/SelectionSort.java
@@ -1,4 +1,17 @@
 package selectionSort;
 
 public class SelectionSort {
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int t = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = t;
+        }
+    }
 }
