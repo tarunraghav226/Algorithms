7a16f2bd2f7e03857b13affda40bb600b59057ea
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-02 16:15:45 +0530

    Added binary search logic.

--- binarySearch/src/binarySearch/BinarySearch.java
+++ binarySearch/src/binarySearch/BinarySearch.java
@@ -1,4 +1,24 @@
 package binarySearch;
 
 public class BinarySearch {
+
+    private static int stepRequired = 0;
+
+    public static int search(int[] arr, int left, int right, int valueToBeSearched) {
+        if (left > right) {
+            return -1;
+        } else {
+            int mid = (left + right) / 2;
+            stepRequired++;
+
+            if (arr[mid] == valueToBeSearched) {
+                System.out.println("Steps Required: " + stepRequired);
+                return mid;
+            } else if (arr[mid] < valueToBeSearched) {
+                return search(arr, mid + 1, right, valueToBeSearched);
+            } else {
+                return search(arr, left, mid - 1, valueToBeSearched);
+            }
+        }
+    }
 }
